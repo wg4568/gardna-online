c6a43220a0579d3c7dc644b4f8406a6a7fbc1ce4
William Gardner <wg4568@gmail.com>
2020-11-03 20:55:24 -0800

    typed encoding functions

--- src/common/helpers/encoding.ts
+++ src/common/helpers/encoding.ts
@@ -1,44 +1,52 @@
-import { assert } from "console";
+// I hate this entire file but it does its job and works just fine
+type BoolByte = [
+    boolean,
+    boolean,
+    boolean,
+    boolean,
+    boolean,
+    boolean,
+    boolean,
+    boolean
+];
 
-function EncodeFloat(value: number): Uint8Array {
+export function EncodeFloat(value: number): Uint8Array {
     return new Uint8Array(new Float32Array([value]).buffer);
 }
 
-function EncodeLongFloat(value: number): Uint8Array {
+export function EncodeLongFloat(value: number): Uint8Array {
     return new Uint8Array(new Float64Array([value]).buffer);
 }
 
-function EncodeUint16(value: number): Uint8Array {
+export function EncodeUint16(value: number): Uint8Array {
     return new Uint8Array(new Uint16Array([value]).buffer);
 }
 
-function EncodeInt16(value: number): Uint8Array {
+export function EncodeInt16(value: number): Uint8Array {
     return new Uint8Array(new Int16Array([value]).buffer);
 }
 
-function EncodeUint32(value: number): Uint8Array {
+export function EncodeUint32(value: number): Uint8Array {
     return new Uint8Array(new Uint32Array([value]).buffer);
 }
 
-function EncodeInt32(value: number): Uint8Array {
+export function EncodeInt32(value: number): Uint8Array {
     return new Uint8Array(new Int32Array([value]).buffer);
 }
 
-function EncodeBoolean(value: boolean): Uint8Array {
+export function EncodeBoolean(value: boolean): Uint8Array {
     return new Uint8Array(new Uint8Array([value ? 255 : 0]).buffer);
 }
 
-function EncodeFlags(values: boolean[]): Uint8Array {
-    assert(values.length == 8, "values must have a length of 8");
-
+export function EncodeFlags(value: BoolByte): Uint8Array {
     var byte: number = 0;
-    for (var i = 0; i < values.length; i++)
-        byte = byte | ((values[i] ? 1 : 0) << (7 - i));
+    for (var i = 0; i < value.length; i++)
+        byte = byte | ((value[i] ? 1 : 0) << (7 - i));
 
     return new Uint8Array([byte]);
 }
 
-function EncodeString(str: string): Uint8Array {
+export function EncodeString(str: string): Uint8Array {
     var array = new Uint8Array(1 + str.length);
     array[0] = str.length;
 
@@ -47,7 +55,7 @@ function EncodeString(str: string): Uint8Array {
     return array;
 }
 
-function EncodeLongString(str: string): Uint8Array {
+export function EncodeLongString(str: string): Uint8Array {
     var array = new Uint8Array(2 + str.length);
     array.set(EncodeInt16(str.length), 0);
 
@@ -56,58 +64,91 @@ function EncodeLongString(str: string): Uint8Array {
     return array;
 }
 
-function DecodeFloat(data: Uint8Array, idx: number) {
+export function DecodeFloat(
+    data: Uint8Array,
+    idx: number
+): { value: number; delta: number } {
     return {
         value: new Float32Array(data.subarray(idx, idx + 4).buffer)[0],
         delta: 4
     };
 }
 
-function DecodeLongFloat(data: Uint8Array, idx: number) {
+export function DecodeLongFloat(
+    data: Uint8Array,
+    idx: number
+): { value: number; delta: number } {
     return {
         value: new Float64Array(data.subarray(idx, idx + 8).buffer)[0],
         delta: 8
     };
 }
 
-function DecodeUint16(data: Uint8Array, idx: number) {
+export function DecodeUint16(
+    data: Uint8Array,
+    idx: number
+): { value: number; delta: number } {
     return {
         value: new Uint16Array(data.subarray(idx, idx + 2).buffer)[0],
         delta: 2
     };
 }
 
-function DecodeInt16(data: Uint8Array, idx: number) {
+export function DecodeInt16(
+    data: Uint8Array,
+    idx: number
+): { value: number; delta: number } {
     return {
         value: new Int16Array(data.subarray(idx, idx + 2).buffer)[0],
         delta: 2
     };
 }
 
-function DecodeUint32(data: Uint8Array, idx: number) {
+export function DecodeUint32(
+    data: Uint8Array,
+    idx: number
+): { value: number; delta: number } {
     return {
         value: new Uint32Array(data.subarray(idx, idx + 4).buffer)[0],
         delta: 4
     };
 }
 
-function DecodeInt32(data: Uint8Array, idx: number) {
+export function DecodeInt32(
+    data: Uint8Array,
+    idx: number
+): { value: number; delta: number } {
     return {
         value: new Int32Array(data.subarray(idx, idx + 4).buffer)[0],
         delta: 4
     };
 }
 
-function DecodeBoolean(data: Uint8Array, idx: number) {
+export function DecodeBoolean(
+    data: Uint8Array,
+    idx: number
+): { value: boolean; delta: number } {
     return {
         value: data[idx] != 0,
         delta: 1
     };
 }
 
-function DecodeFlags(data: Uint8Array, idx: number) {
+export function DecodeFlags(
+    data: Uint8Array,
+    idx: number
+): { value: BoolByte; delta: number } {
     var number = data[idx];
-    var flags: boolean[] = [];
+    var flags: BoolByte = [
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false
+    ];
 
     for (var i = 0; i < 8; i++) flags[i] = (number & (1 << (7 - i))) != 0;
 
@@ -117,7 +158,10 @@ function DecodeFlags(data: Uint8Array, idx: number) {
     };
 }
 
-function DecodeString(data: Uint8Array, idx: number) {
+export function DecodeString(
+    data: Uint8Array,
+    idx: number
+): { value: string; delta: number } {
     var str = "";
     var length = data[idx];
 
@@ -130,7 +174,10 @@ function DecodeString(data: Uint8Array, idx: number) {
     };
 }
 
-function DecodeLongString(data: Uint8Array, idx: number) {
+export function DecodeLongString(
+    data: Uint8Array,
+    idx: number
+): { value: string; delta: number } {
     var str = "";
     var length = DecodeUint16(data, idx).value;
 
